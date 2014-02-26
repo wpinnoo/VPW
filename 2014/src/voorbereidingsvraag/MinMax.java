94fa7d1446ec9d75aa64fb24980e2b4d7caa3e2c
Wouter Pinnoo <pinnoo.wouter@gmail.com>
2014-02-26 18:48:08 +0100

    voorbereidingsvraag 2014

---
+++ 2014/src/voorbereidingsvraag/MinMax.java
@@ -0,0 +1,34 @@
+package voorbereidingsvraag;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Wouter Pinnoo <Wouter.Pinnoo@UGent.be>
+ */
+public class MinMax {
+ 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int lijsten = sc.nextInt();
+        
+        for(int i=0; i < lijsten; i++){
+            int grootte = sc.nextInt();
+            
+            int min = Integer.MAX_VALUE;
+            int max = Integer.MIN_VALUE;
+            
+            for(int j=0; j < grootte; j++){
+                int getal = sc.nextInt();
+                if(getal < min){
+                    min = getal;
+                }
+                if(getal > max){
+                    max = getal;
+                }
+            }
+            
+            System.out.println((i+1) + " " + min + " " + max);
+        }
+    }
+}
